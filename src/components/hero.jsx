1ab9c1c89d60d1d7688a9b42a97965dda0564e6e
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-09 18:01:50 +0300

    styling codes for hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin, faWhatsapp, faFacebook, faTiktok } from "@fortawesome/free-brands-svg-icons";
+import codie from '../assets/codegirl.jpg';
 
 function Hero() {
   return (
-    <div className='min-h-[100vh] scroll-smooth'>
+    <div className='flex justify-center items-center scroll-smooth'>
     <div>
-        <h3 className="text-gray-700">Hello 👋,</h3>
-        <h1 className="text-purple-600 text-5xl mt-2">I'm Linkon Tuhoto</h1>
-        <h3 className="text-purple-700 text-3xl mt-1">And I am a Frontend Developer</h3>
+        <h3 className="text-gray-700 text-[3.2rem] ">Hello 👋,</h3>
+        <h1 className="text-purple-600 text-5xl mt-2 text-[5.4rem] font-bold leading-13">I'm Linkon Tuhoto</h1>
+        <h3 className="text-purple-700 text-3xl mt-1 mb-6">And I am a <span className='text-purple-400'>Frontend Developer</span></h3>
         <p className="text-gray-700 mt-4 ">I build responsive and user-friendly websites that bring ideas to life.  
            Passionate about clean code, modern design, and creating seamless user experiences.
         </p>
@@ -22,8 +23,8 @@ function Hero() {
         <button className="bg-purple-600 hover:bg-purple-700 text-white font-semibold px-6 py-3 rounded-lg"><a href="#">Download CV</a></button>
     </div>
 
-    <div>
-        <img src="#" alt="person" />
+    <div className="mx-auto w-64 h-64 rounded-2xl shadow-lg animate-float">
+        <img src={codie} className="" alt="person" />
     </div>
 
     </div>
