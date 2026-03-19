c4017bdcfbcab903ac6a12e235166c7cfd6fa730
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-19 08:03:52 +0300

    styled the project card

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -17,6 +17,18 @@ function Projects() {
 
         <div className='bg-purple-50 p-6 rounded-2xl border border-purple-200 shadow-sm hover:shadow-[0_10px_30px_rgba(168, 85, 247,0.2)]'>
             <img src="" alt="picture" />
+            <h2 className='text-gray-900 font-semibold text-xl mb-1'>E-commerce Website</h2>
+            <p className='text-gray-700 text-sm mb-4'>A fully responsive e-commerce platform with product listings, interactive UI and seamless shopping experience for users</p>
+            <div className='flex flex-wrap gap-2 mb-4'>
+                    <span className='bg-white text-purple-600 border border-purple-200 px-3 py-1 text-xs rounded-full'>HTML</span>
+                    <span className='bg-white text-purple-600 border border-purple-200 px-3 py-1 text-xs rounded-full'>CSS</span>
+                    <span className='bg-white text-purple-600 border border-purple-200 px-3 py-1 text-xs rounded-full'>JAVASCRIPT</span>
+            </div>
+            <button className='inline-block bg-purple-500 text-white hover:bg-purple-600 transition-all duration-300 px-5 py-2 font-medium text-sm'><a href="">View Project</a></button>
+        </div>
+
+        <div className='bg-purple-50 p-6 rounded-2xl border border-purple-200 shadow-sm hover:shadow-[0_10px_30px_rgba(168, 85, 247,0.2)]'>
+            <img src="" alt="picture" />
             <h2 className='text-gray-900 font-semibold text-xl mb-1'>Hotel Booking Website</h2>
             <p className='text-gray-700 text-sm mb-4'>A hotel website featuring the meals offered and the details of the hotel based on its locality. It is elegant user interface designed to enhance user experience</p>
             <div className='flex flex-wrap gap-2 mb-4'>
