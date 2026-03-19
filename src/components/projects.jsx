38809799f30a132b50ab40a375b17be7f67ffc76
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-19 08:44:35 +0300

    styled card and styled services section

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div className='bg-white'>
-        <div className='bg-purple-50 p-6 rounded-2xl border border-purple-200 shadow-sm hover:shadow-[0_10px_30px_rgba(168, 85, 247,0.2)]'>
+    <div className='bg-white grid grid-cols-1 md:grid-cols-3 gap-10 p-15'>
+        <div className='bg-purple-50 p-6 rounded-2xl border border-purple-200 shadow-sm hover:shadow-[0_10px_30px_rgba(168, 85, 247,0.9)]'>
             <img src="" alt="picture" />
             <h2 className='text-gray-900 font-semibold text-xl mb-1'>School Management Website</h2>
             <p className='text-gray-700 text-sm mb-4'>A dynamic School platform showcasing academic programs, facilities, and services, designed to provide clear and accessible information to students and parents</p>
@@ -15,7 +15,7 @@ function Projects() {
             <button className='inline-block bg-purple-500 text-white hover:bg-purple-600 transition-all duration-300 px-5 py-2 font-medium text-sm'><a href="">View Project</a></button>
         </div>
 
-        <div className='bg-purple-50 p-6 rounded-2xl border border-purple-200 shadow-sm hover:shadow-[0_10px_30px_rgba(168, 85, 247,0.2)]'>
+        <div className='bg-purple-50 p-6 rounded-2xl border border-purple-200 shadow-sm hover:shadow-[0_10px_30px_rgba(168, 85, 247,0.9)]'>
             <img src="" alt="picture" />
             <h2 className='text-gray-900 font-semibold text-xl mb-1'>E-commerce Website</h2>
             <p className='text-gray-700 text-sm mb-4'>A fully responsive e-commerce platform with product listings, interactive UI and seamless shopping experience for users</p>
@@ -27,7 +27,7 @@ function Projects() {
             <button className='inline-block bg-purple-500 text-white hover:bg-purple-600 transition-all duration-300 px-5 py-2 font-medium text-sm'><a href="">View Project</a></button>
         </div>
 
-        <div className='bg-purple-50 p-6 rounded-2xl border border-purple-200 shadow-sm hover:shadow-[0_10px_30px_rgba(168, 85, 247,0.2)]'>
+        <div className='bg-purple-50 p-6 rounded-2xl border border-purple-200 shadow-sm hover:shadow-[0_10px_30px_rgba(168, 85, 247,0.9)]'>
             <img src="" alt="picture" />
             <h2 className='text-gray-900 font-semibold text-xl mb-1'>Hotel Booking Website</h2>
             <p className='text-gray-700 text-sm mb-4'>A hotel website featuring the meals offered and the details of the hotel based on its locality. It is elegant user interface designed to enhance user experience</p>
