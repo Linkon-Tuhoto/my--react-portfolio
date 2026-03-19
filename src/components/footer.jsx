7c27d3705a4f63c34e7612469d925d1ba80ddab4
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-19 19:37:54 +0300

    styled the copyright

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-        <p>&copy; {new Date().getFullYear()} Linkon Tuhoto. All rights reserved</p>
+    <div className='bg-gray-900 text-gray-400 text-center py-6 border-t border-white mb-0'>
+        <p className='text-sm'>&copy; {new Date().getFullYear()} Linkon Tuhoto. All rights reserved</p>
     </div>
   )
 }
