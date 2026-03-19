5a386e5b29790cc5b6fae88470d1d7bd48956e37
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-19 07:22:08 +0300

    created first project card layout

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>P</div>
+    <div>
+        <div>
+            <img src="" alt="picture" />
+            <h2>Hotel Booking Website</h2>
+            <p>A hotel website featuring the meals offered and the details of the hotel based on its locality. It is elegant user interface designed to enhance user experience</p>
+            <div>
+                <div>
+                    <h4>HTML</h4>
+                </div>
+                
+                <div>
+                    <h4>CSS</h4>
+                </div>
+            </div>
+            <button><a href="">View Project</a></button>
+        </div>
+    </div>
   )
 }
 
