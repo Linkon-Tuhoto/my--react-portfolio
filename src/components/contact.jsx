24d3d73120cc9a7e5e6fbe5c9aae8a90e29bbe78
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-19 11:15:24 +0300

    styled contact form-input colors

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,16 +4,16 @@ function Contact() {
   return (
     <div>
         <h2>Contact Me</h2>
-        <form action="POST" className='w-[70rem] m-4 text-center mb-12'>
-            <div className='flex justify-between flex-wrap'>
-                <input type="text" placeholder='Full Name' className='w-[49%] p-6 size-6.5 bg-gray-50 m-[7rem 0] rounded-[8rem]'/>
-                <input type="email" id="mail" placeholder='Email Address' className='w-[49%] p-6 size-6.5 bg-gray-50 m-[7rem 0] rounded-[8rem]' />
+        <form action="POST" className='w-[70rem] m-4 mb-12'>
+            <div className='flex justify-between items-center flex-wrap p-3'>
+                <input type="text" placeholder='Full Name' className='w-[49%] p-6 size-6.5 bg-green-400 m-[.7rem 0] rounded-[.8rem]'/>
+                <input type="email" id="mail" placeholder='Email Address' className='w-[49%] p-6 size-6.5 bg-green-400 m-[.7rem 0] rounded-[.8rem]' />
             </div>
-            <div className='flex justify-between flex-wrap'>
-                <input type="number" placeholder='Phone Number' className='w-[49%] p-6 size-6.5 bg-gray-50  m-[7rem 0] rounded-[8rem]' />
-                <input type="text" placeholder='Email Subject' className='w-[49%] p-6 size-6.5 bg-gray-50 m-[7rem 0] rounded-[8rem]' />
+            <div className='flex justify-between items-center flex-wrap p-3'>
+                <input type="number" placeholder='Phone Number' className='w-[49%] p-6 size-6.5 bg-green-400  m-[.7rem 0] rounded-[.8rem]' />
+                <input type="text" placeholder='Email Subject' className='w-[49%] p-6 size-6.5 bg-green-400 m-[.7rem 0] rounded-[.8rem]' />
             </div>
-            <textarea name="" id="" rows="10" cols="30"></textarea>
+            <textarea name="" id="" rows="10" cols="30" className='w-[100%] bg-green-400  m-[.7rem 0] mt-3 rounded-[.8rem]'></textarea>
             <button type="submit">Submit</button>
         </form>
     </div>
