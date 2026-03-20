4c457b564875a94c8ff4e2610485eef8b062eb09
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-20 09:48:07 +0300

    added styling for hovering on card 1 in services section

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -10,15 +10,17 @@ function Services() {
     <section id="services" className='scroll-mt-26'>
     <div className='bg-gray-50'>
         <h2 className='text-center font-bold text-3xl md:text-4xl p-4 pt-8'>My Services</h2>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-10 p-20'>
-        <div className='group relative border text-center p-10 border-gray-200 origin-bottom shadow-md hover:shadow-purple-200 hover:shadow-lg hover:scale-105 transition-all duration-300 bg-white/80 backdrop-blur-md'>
-        <div className='absolute inset-0 bg-purple-500 opacity-0 group-hover:opacity-100 transition-all duration-500'></div>
-            <div className='group-hover:text-white transition'><FontAwesomeIcon icon={faGithub} className='mb-4 text-purple-500 text-4xl group-hover:text-white transition' /></div>
-            <h3 className='font-medium text-lg mb-2 group-hover:text-white transition'>Web Development</h3>
-            <p className='text-gray-600 md:opacity-0 md:max-h-0 overflow-hidden transition-all duration-500 md:group-hover:opacity-100 md:group-hover:max-h-40 md:group-hover:translate-y-0'>I build responsive and high-performance websites using modern technologies, 
+        <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-10 p-20'>
+        <div className='group md:relative border text-center p-10 border-gray-200 origin-bottom shadow-md hover:shadow-purple-200 hover:shadow-lg hover:scale-105 transition-all duration-300 bg-white/80 backdrop-blur-md'>
+        {/*<div className='absolute inset-0 bg-purple-500 opacity-0 group-hover:opacity-100 transition-all duration-500'></div>*/}
+            <FontAwesomeIcon icon={faGithub} className='mb-4 text-purple-500 text-4xl md:text-9xl' />
+            <h3 className='font-medium text-lg mb-2 md:text-4xl'>Web Development</h3>
+            <div className='md:absolute md:w-full md:bottom-0 md:left-0 md:w-full md:h-0 md:overflow-hidden transition-all duration-500 md:bg-gradient-to-t md:from-purple-900/80 md:via-purple-700/50 md:to-transparent md:absolute md:group-hover:h-[100%] md:flex md:flex-col jmd:ustify-center md:items-center md:text-center md:mt-4'>
+            <p className='text-gray-600 md:text-2xl'>I build responsive and high-performance websites using modern technologies, 
                 ensuring seamless functionality across all devices.
             </p>
-            <button className='bg-purple-500 inline-block px-5 py-2 text-white rounded-full font-medium text-sm mt-4 hover:bg-purple-600 transition-all duration-300'>Read More</button>
+            <button className='bg-purple-500 inline-block px-5 py-2 text-white rounded-full font-medium text-sm mt-4 hover:bg-purple-600 transition-all duration-300 md:text-4xl'>Read More</button>
+            </div>
         </div>
 
         <div className='border text-center p-10 border-gray-200 origin-bottom shadow-md hover:shadow-purple-200 hover:shadow-lg hover:scale-105 transition-all duration-300 bg-white/80 backdrop-blur-md'>
@@ -39,7 +41,7 @@ function Services() {
             <button className='bg-purple-500 inline-block px-5 py-2 text-white rounded-full font-medium text-sm mt-4 hover:bg-purple-600 transition-all duration-300'>Read More</button>
         </div>
         </div>
-    </div>
+        </div>
     </section>
   )
 }
