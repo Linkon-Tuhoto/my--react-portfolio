fef8b381393d8951a82b82ca4dfe7a916dacd67c
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-21 14:15:14 +0300

    minor styling changes

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -8,10 +8,10 @@ function Hero() {
   return (
     <section id="home" className='scroll-mt-26'>
     <div className='grid grid-cols-1 md:flex md:justify-center md:items-center scroll-smooth pt-[10%] md:pt-[3%] '>
-    <div className='text-center p-8 md:p-16'>
-        <h3 className="text-gray-700 text-[3.2rem] font-normal leading-relaxed text-lg ">Hello 👋,</h3>
-        <h1 className="text-purple-600 text-5xl mt-2 text-[3.2rem] md:text-[4.4rem] font-bold leading-13">I'm Linkon Tuhoto</h1>
-        <h3 className="text-purple-700 text-3xl md:text-4xl mt-1 mb-6 leading-snug font-bold">And I am a {" "} <span className="text-purple-400">Frontend Developer</span></h3>
+    <div className='text-center p-8 md:p-16 leading-relaxed'>
+        <h3 className="text-[#9CA3AF] text-[3.2rem] font-normal leading-relaxed text-[22px] ">Hello 👋,</h3>
+        <h1 className="text-[#7C3AED] text-5xl mt-2 text-[3.2rem] md:text-[4rem] font-extrabold leading-13">I'm Linkon Tuhoto</h1>
+        <h3 className="text-[#6B7280] text-3xl md:text-3xl mt-1 mb-6 leading-snug font-normal">And I am a <span className='text-[ #7C3AED] font-semibold'>Frontend Developer</span></h3>
         <p className="text-gray-700 mt-4 text-base md:text-lg font-normal leading-relaxed mx-auto max-w-[800px]">I build responsive and user-friendly websites that bring ideas to life.  
            Passionate about clean code, modern design, and creating seamless user experiences.
         </p>
@@ -22,11 +22,11 @@ function Hero() {
             <a href="#" alt="icon" target="_blank"><FontAwesomeIcon icon={faTiktok} className="text-2xl mx-2"  /></a>
             <a href="#" alt="icon" target="_blank"><FontAwesomeIcon icon={faLinkedin} className="text-2xl mx-2"  /></a>
         </div>
-        <button className="bg-purple-600 hover:bg-purple-700 text-white font-semibold px-6 py-3 rounded-lg transition-colors mt-6 inline-block"><a href="#">Download CV</a></button>
+        <button className="bg-purple-600 hover:bg-purple-700 text-white font-semibold px-8 py-3 rounded-[9999px] transition-colors mt-6 inline-block text-2xl"><a href="#">Download CV</a></button>
     </div>
 
     <div className="w-[16rem] h-[16rem] mx-auto md:mx-0">
-        <img src={codie} className="mx-auto rounded-2xl shadow-lg animate-float w-full h-full object-cover" alt="person"  />
+        <img src={codie} className="mx-auto rounded-full shadow-lg animate-float w-full h-full object-cover shadow-[0 0 40px rgba(124,58,237,0.25)] border-3 border-[#7C3AED]" alt="person"  />
     </div>
 
     </div> 
