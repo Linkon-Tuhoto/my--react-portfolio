eb0e422196fcf96670a5b39b8fa4c847f076578b
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-26 17:14:47 +0300

    minor-styling in fonts

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -15,7 +15,7 @@ function Services() {
             <FontAwesomeIcon icon={faCode} className='mb-4 text-purple-500 text-4xl md:text-9xl' />
             <h3 className='font-semibold text-xl mb-2 md:text-4xl'>Web Development</h3>
             <div className='md:absolute md:w-full md:bottom-0 md:left-0 md:h-0 md:overflow-hidden transition-all duration-500 md:bg-gradient-to-t md:from-purple-900/80 md:via-purple-700/50 md:to-transparent md:group-hover:h-[100%] md:flex md:flex-col jmd:ustify-center md:items-center md:text-center md:mt-4'>
-            <p className='text-gray-600 md:text-2xl'>I build responsive and high-performance websites using modern technologies, 
+            <p className='text-gray-600 text-sm md:text-2xl'>I build responsive and high-performance websites using modern technologies, 
                 ensuring seamless functionality across all devices.
             </p>
             <button className='bg-purple-500 inline-block px-5 py-2 text-white rounded-full font-medium text-sm mt-4 hover:bg-purple-600 transition-all duration-300 md:text-3xl'>Read More</button>
@@ -26,7 +26,7 @@ function Services() {
             <FontAwesomeIcon icon={faLayerGroup} className='mb-4 text-purple-500 text-4xl md:text-9xl' />
             <h3 className='font-medium text-lg mb-2 md:text-4xl'>UI/UX Design</h3>
             <div className='md:absolute md:w-full md:bottom-0 md:left-0 md:h-0 md:overflow-hidden transition-all duration-500 md:bg-gradient-to-t md:from-purple-900/80 md:via-purple-700/50 md:to-transparent md:group-hover:h-[100%] md:flex md:flex-col jmd:ustify-center md:items-center md:text-center md:mt-4'>
-            <p className='text-gray-600 md:text-2xl'>
+            <p className='text-gray-600 text-sm md:text-2xl'>
                 I design clean and user-friendly interfaces focused on delivering intuitive and engaging user experiences.
             </p>
             <button className='bg-purple-500 inline-block px-5 py-2 text-white rounded-full font-medium text-sm mt-4 hover:bg-purple-600 transition-all duration-300 md:text-3xl'>Read More</button>
@@ -37,7 +37,7 @@ function Services() {
             <FontAwesomeIcon icon={faTools} className='mb-4 text-purple-500 text-4xl md:text-9xl' />
             <h3 className='font-medium text-lg mb-2 md:text-4xl'>Maintenance & Support</h3>
             <div className='md:absolute md:w-full md:bottom-0 md:left-0 md:h-0 md:overflow-hidden transition-all duration-500 md:bg-gradient-to-t md:from-purple-900/80 md:via-purple-700/50 md:to-transparent md:group-hover:h-[100%] md:flex md:flex-col jmd:ustify-center md:items-center md:text-center md:mt-4'>
-            <p className='text-gray-600 md:text-2xl'>
+            <p className='text-gray-600 text-sm md:text-2xl'>
                 I provide ongoing support and updates to keep websites secure, functional, and up to date.
             </p>
             <button className='bg-purple-500 inline-block px-5 py-2 text-white rounded-full font-medium text-sm mt-4 hover:bg-purple-600 transition-all duration-300 md:text-3xl'>Read More</button>
