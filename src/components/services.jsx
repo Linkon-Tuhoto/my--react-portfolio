e04b4f50c7ab81fe65e5a7825ef9048439b680d6
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-26 14:59:37 +0300

    styling codes change in services section

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -13,8 +13,8 @@ function Services() {
         <div className='group md:relative border text-center p-10 border-gray-200 origin-bottom shadow-md hover:shadow-purple-200 hover:shadow-lg hover:scale-105 transition-all duration-300 bg-white/80 backdrop-blur-md'>
         {/*<div className='absolute inset-0 bg-purple-500 opacity-0 group-hover:opacity-100 transition-all duration-500'></div>*/}
             <FontAwesomeIcon icon={faCode} className='mb-4 text-purple-500 text-4xl md:text-9xl' />
-            <h3 className='font-medium text-lg mb-2 md:text-4xl'>Web Development</h3>
-            <div className='md:absolute md:w-full md:bottom-0 md:left-0 md:w-full md:h-0 md:overflow-hidden transition-all duration-500 md:bg-gradient-to-t md:from-purple-900/80 md:via-purple-700/50 md:to-transparent md:absolute md:group-hover:h-[100%] md:flex md:flex-col jmd:ustify-center md:items-center md:text-center md:mt-4'>
+            <h3 className='font-semibold text-xl mb-2 md:text-4xl'>Web Development</h3>
+            <div className='md:absolute md:w-full md:bottom-0 md:left-0 md:h-0 md:overflow-hidden transition-all duration-500 md:bg-gradient-to-t md:from-purple-900/80 md:via-purple-700/50 md:to-transparent md:group-hover:h-[100%] md:flex md:flex-col jmd:ustify-center md:items-center md:text-center md:mt-4'>
             <p className='text-gray-600 md:text-2xl'>I build responsive and high-performance websites using modern technologies, 
                 ensuring seamless functionality across all devices.
             </p>
@@ -25,7 +25,7 @@ function Services() {
         <div className='group md:relative border text-center p-10 border-gray-200 origin-bottom shadow-md hover:shadow-purple-200 hover:shadow-lg hover:scale-105 transition-all duration-300 bg-white/80 backdrop-blur-md'>
             <FontAwesomeIcon icon={faLayerGroup} className='mb-4 text-purple-500 text-4xl md:text-9xl' />
             <h3 className='font-medium text-lg mb-2 md:text-4xl'>UI/UX Design</h3>
-            <div className='md:absolute md:w-full md:bottom-0 md:left-0 md:w-full md:h-0 md:overflow-hidden transition-all duration-500 md:bg-gradient-to-t md:from-purple-900/80 md:via-purple-700/50 md:to-transparent md:absolute md:group-hover:h-[100%] md:flex md:flex-col jmd:ustify-center md:items-center md:text-center md:mt-4'>
+            <div className='md:absolute md:w-full md:bottom-0 md:left-0 md:h-0 md:overflow-hidden transition-all duration-500 md:bg-gradient-to-t md:from-purple-900/80 md:via-purple-700/50 md:to-transparent md:group-hover:h-[100%] md:flex md:flex-col jmd:ustify-center md:items-center md:text-center md:mt-4'>
             <p className='text-gray-600 md:text-2xl'>
                 I design clean and user-friendly interfaces focused on delivering intuitive and engaging user experiences.
             </p>
@@ -36,7 +36,7 @@ function Services() {
         <div className='group md:relative border text-center p-10 border-gray-200 origin-bottom shadow-md hover:shadow-purple-200 hover:shadow-lg hover:scale-105 transition-all duration-300 bg-white/80 backdrop-blur-md'>
             <FontAwesomeIcon icon={faTools} className='mb-4 text-purple-500 text-4xl md:text-9xl' />
             <h3 className='font-medium text-lg mb-2 md:text-4xl'>Maintenance & Support</h3>
-            <div className='md:absolute md:w-full md:bottom-0 md:left-0 md:w-full md:h-0 md:overflow-hidden transition-all duration-500 md:bg-gradient-to-t md:from-purple-900/80 md:via-purple-700/50 md:to-transparent md:absolute md:group-hover:h-[100%] md:flex md:flex-col jmd:ustify-center md:items-center md:text-center md:mt-4'>
+            <div className='md:absolute md:w-full md:bottom-0 md:left-0 md:h-0 md:overflow-hidden transition-all duration-500 md:bg-gradient-to-t md:from-purple-900/80 md:via-purple-700/50 md:to-transparent md:group-hover:h-[100%] md:flex md:flex-col jmd:ustify-center md:items-center md:text-center md:mt-4'>
             <p className='text-gray-600 md:text-2xl'>
                 I provide ongoing support and updates to keep websites secure, functional, and up to date.
             </p>
